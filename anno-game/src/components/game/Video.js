537a51e665820ffec6f6053943a3d9b1dd39a3e7
amy011872 <amy011872@gmail.com>
2022-12-21 22:15:30 +0800

    video fixed!!!

--- anno-game/src/components/game/Video.js
+++ anno-game/src/components/game/Video.js
@@ -2,9 +2,13 @@
 export default function Video({filename}) {
     return (
       <>
-        <video controls width="55%" className="pad-little">
-          <source src={filename} type="video/mp4" />
-        </video>
+        <video 
+         controls
+         width="55%"
+         className="pad-little"
+         src={filename}
+         type="video/mp4"
+        />
       </>
     );
   };

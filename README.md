16059911c4225f8fa3ee00f747538647e8394848
amy011872 <amy011872@gmail.com>
2022-12-22 11:48:18 +0800

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # NlpWeb-Anno-Game
+
+ This is a MOCO challenge!!! 
+什麼是MOCO？MOCO是取自台大語言所近期釋出的多模態語料庫：MultiMoco NTU corpus (https://multimoco.linguistics.ntu.edu.tw/index.html#content-stat )
+本遊戲擷取一些立委及政治人物的質詢影片，做成去識別化的影片，讓玩家可以專注在說話者手勢的運用——
+你對不同模態的語言訊息夠敏銳嗎？是否有信心可以完成挑戰呢？
+（創作者：台大語言所 周昕妤、王伯雅)
--- anno-game/src/components/About.js
+++ anno-game/src/components/About.js
@@ -16,13 +16,15 @@ export default function About(){
           justifyContent: "center",
         }}
       >
-        <Typography variant="h3" gutterBottom>About Moco Game</Typography>
+        <Typography variant="h3" gutterBottom>關於本遊戲</Typography>
         <div className="container-md center gutter">
           <p >
           This is a MOCO challenge!!! 
           什麼是MOCO？MOCO是取自台大語言所近期釋出的多模態語料庫：<a href="https://multimoco.linguistics.ntu.edu.tw/index.html#content-stat"> MultiMoco NTU corpus</a><br/>
           本遊戲擷取一些立委及政治人物的質詢影片，做成去識別化的影片，讓玩家可以專注在說話者手勢的運用——<br/>
           你對不同模態的語言訊息夠敏銳嗎？是否有信心可以完成挑戰呢？<br/>
+          （創作者：台大語言所 周昕妤、王伯雅）<br/>
+         React project - <a href="https://github.com/amy011872/NlpWeb-Anno-Game">github</a>
           </p>
         </div>
         <div className="container-lg center">
